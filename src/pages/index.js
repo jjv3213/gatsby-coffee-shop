5b3a205f5b779813adaa6c232c9acb466ddb6d71
joelv <joelv3213@gmail.com>
2019-05-07 08:41:44 -0600

    background section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,30 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import BackgroundSection from "../components/global/BackgroundSection"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="Coffee Shop"
+    />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
